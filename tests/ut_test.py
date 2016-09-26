e9a1d79ccec022bdf05fb551825a757ea7d48042
Rohan Bhiangi <rkb332@nyu.edu>
2016-09-26 10:54:11 -0400

    ut_test.py

--- tests/ut_test.py
+++ tests/ut_test.py
@@ -17,30 +17,38 @@ to the Arduino board through this python script.
 import serial
 import time
 
-#Use Py Serial library for communicating with the Arduino
-#For mac users, the port should be '/dev/cu.usbmodem1421'
-#or '/dev/tty.usbmodem1421'
-#For Windows, the port should be COM1 or something similar
+"""
+Use Py Serial library for communicating with the Arduino
+For mac users, the port should be '/dev/cu.usbmodem1421'
+or '/dev/tty.usbmodem1421'
+For Windows, the port should be COM1 or something similar
+"""
 ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
 
-#This funcation rotates the turn table clockwise for
-#3 seconds and calls the dummy get_acceleration function
+"""
+This funcation rotates the turn table clockwise for
+3 seconds and calls the dummy get_acceleration function
+"""
 def turn_table_right():
 	ser.write("y")
 	print "in right"
 	print get_acceleration()
 	time.sleep(3)
 
-#This funcation rotates the turn table anit-clockwise for
-#3 seconds and calls the dummy get_acceleration function
+"""
+This funcation rotates the turn table anit-clockwise for
+3 seconds and calls the dummy get_acceleration function
+"""
 def turn_table_left():
 	ser.write("n")
 	print "in left"
 	print get_acceleration()
 	time.sleep(3)
 
-#A dummy acceleration function since we are not yet able
-#to access Sensibility Sensor APIs through Python
+"""
+A dummy acceleration function since we are not yet able
+to access Sensibility Sensor APIs through Python
+"""
 def get_acceleration():
 	return 0.0,0.0,9.8
 
